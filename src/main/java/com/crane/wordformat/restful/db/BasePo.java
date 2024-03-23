0fc8f471e9445c5181780db88490eb800338ad00
qingpei168861 <1915986618@qq.com>
2024-03-23 20:05:52 +0800

    修改

--- src/main/java/com/crane/wordformat/restful/db/BasePo.java
+++ src/main/java/com/crane/wordformat/restful/db/BasePo.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.io.Serial;
@@ -26,12 +27,14 @@ public class BasePo implements Serializable {
   private String id;
 
   @TableField(fill = FieldFill.INSERT)
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime createdTime;
 
   @TableField(fill = FieldFill.INSERT)
   private String createdBy;
 
   @TableField(fill = FieldFill.UPDATE)
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime updatedTime;
 
   @TableField(fill = FieldFill.UPDATE)
