0894b392590aa204b04c4523324a414be03fd5db
qingpei168861 <1915986618@qq.com>
2024-03-26 02:34:48 +0800

    修改封面文件预览bug

--- web/src/pages/formatter/cover-form/index.vue
+++ web/src/pages/formatter/cover-form/index.vue
@@ -45,14 +45,14 @@ const previewLoading = ref(false);
 const visible = ref(false);
 let images: string[] = [];
 async function preview(row: any) {
-  images.length = 0;
-  visible.value = true;
+  visible.value = false;
+  images = [];
   const urls = await fileApi.getPictureURL(row.coverPreviewUrl.toString());
   for (let i = 0; i < urls.length; i++) {
     images.push(urls[i]);
-    // images.push("https://tdesign.gtimg.com/demo/demo-image-1.png");
   }
-  console.log(images);
+  console.log(images)
+  visible.value = true;
 }
 
       const downloadLoading = ref([]);
@@ -132,83 +132,12 @@ async function preview(row: any) {
     </template>
   </CrudPage>
 
-  <t-image-viewer v-model:visible="visible" :images="images" >
-  </t-image-viewer>
+  <div>
+    <t-image-viewer v-model:visible="visible" :images="images">
+    </t-image-viewer>
+  </div>
+
 </template>
 
 <style lang="less"  scoped>
-.tdesign-demo-image-viewer__ui-image {
-  width: 100%;
-  height: 100%;
-  display: inline-flex;
-  position: relative;
-  justify-content: center;
-  align-items: center;
-  border-radius: var(--td-radius-small);
-  overflow: hidden;
-}
-
-.tdesign-demo-image-viewer__ui-image--hover {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: 0;
-  top: 0;
-  opacity: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-  color: var(--td-text-color-anti);
-  line-height: 22px;
-  transition: 0.2s;
-}
-
-.tdesign-demo-image-viewer__ui-image:hover .tdesign-demo-image-viewer__ui-image--hover {
-  opacity: 1;
-  cursor: pointer;
-}
-
-.tdesign-demo-image-viewer__ui-image--img {
-  width: auto;
-  height: auto;
-  max-width: 100%;
-  max-height: 100%;
-  cursor: pointer;
-  position: absolute;
-}
-
-.tdesign-demo-image-viewer__ui-image--footer {
-  padding: 0 16px;
-  height: 56px;
-  width: 100%;
-  line-height: 56px;
-  font-size: 16px;
-  position: absolute;
-  bottom: 0;
-  color: var(--td-text-color-anti);
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 100%);
-  display: flex;
-  box-sizing: border-box;
-}
-
-.tdesign-demo-image-viewer__ui-image--title {
-  flex: 1;
-}
-
-.tdesign-demo-popup__reference {
-  margin-left: 16px;
-}
-
-.tdesign-demo-image-viewer__ui-image--icons .tdesign-demo-icon {
-  cursor: pointer;
-}
-
-.tdesign-demo-image-viewer__base {
-  width: 160px;
-  height: 160px;
-  margin: 10px;
-  border: 4px solid var(--td-bg-color-secondarycontainer);
-  border-radius: var(--td-radius-medium);
-}
 </style>
